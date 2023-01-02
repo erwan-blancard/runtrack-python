d9940a852d7aac8d9af2cfdb2008966383e9c39f
erwan-blancard <erwan.blancard@laplateforme.io>
2023-01-02 11:21:30 +0100

    commit des jobs 2 au bonus

---
+++ jour01/bonus/character-checker.py
@@ -0,0 +1,6 @@
+print("Entrez une chaîne de caractères :")
+input_string = input()
+if input_string.count("e") > 0:
+    print("Cette chaîne contient le caractère \'e\'.")
+else:
+    print("Il n'y a pas le caractère \'e\' dans cette chaîne.")
