3f9ff65c8024662f8991b8bd5dc49e1a67786c14
erwan-blancard <erwan.blancard@laplateforme.io>
2023-01-05 09:32:44 +0100

    changements mineurs dans le job 6 du jour 3

--- jour03/job06/pyramide.py
+++ jour03/job06/pyramide.py
@@ -1,15 +1,11 @@
 def pyramide():
     alphabet = "abcdefghijklmnopqrstuvwxyz" * 10
     i = 0
-    chars_count = 0
     total_chars = 0
     str_to_print = ""
-    while chars_count <= (len(alphabet) - total_chars):
-        chars_count = 0
-
+    while i <= (len(alphabet) - total_chars):
         for j in range(total_chars, total_chars + i +1):
             str_to_print = str_to_print + alphabet[j]
-            chars_count += 1
         print(str_to_print)
 
         i += 1
